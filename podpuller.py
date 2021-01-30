c7ffe2a2aacda1a5fc519d9ac1b61bebde9bf303
Guy Hoffman <hoffman@cornell.edu>
2021-01-30 17:49:43 -0500

    Initial Commit

---
+++ podpuller.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+
+from ui import yesno
+import feedparser
+
+import configparser
+import logging
+from os.path import expanduser
+from subprocess import run
+from termcolor import cprint
+
+from process import *
+
+# Configuration Filename 
+config_filename = '~/.config/podpuller/feeds.conf'
+directories = {}
+
+logging.basicConfig(level=logging.INFO)
+
+def update_configfile(conf):
+    with open(expanduser(config_filename), 'w') as configfile:
+        conf.write(configfile)
+
+def process_feed(feed_name, conf, be_quick):
+    """Process Single Feed
+
+    Args:
+        feed_name (string): Name of feed to process
+        conf (ConfigParser): We pass the config parser to update the file with RSS
+         info and to read the defaults  
+    """
+    feed = conf[feed_name]
+    url = feed['URL']
+    keep = feed.getint('keep_episodes', 0)
+    
+    rss = feedparser.parse(url)
+
+    # Check for fetch / parse errors
+    try:
+        check_feederrors(rss)
+    except Exception as e:
+        logging.error("Erroneous feed URL: %s (%s)"%(url, type(e)))
+        return
+
+    cprint(f'  ======== {rss.feed.title} ({feed.name}/{keep}) ========  ', 'grey', 'on_yellow')
+
+    # Oldest or newest first
+    if feed.getboolean('oldest_first'):
+        rss.entries.reverse()
+
+    #################################################
+    # This is where the logic happens. Right now
+    # single logic: always have the latest [keep] episodes
+    #################################################
+    dl_dir = directories['dl']
+    
+    tobe_deleted = []
+    if not be_quick:
+        tobe_deleted = ui.mark_deletion(dl_dir + os.sep + feed.name)
+        
+    have = 0
+    for episode in rss.entries:
+        hash_episode(episode)
+
+        if tobe_deleted and generate_filename(episode) in tobe_deleted:
+            delete_episode(feed.name, episode, dl_dir)
+            markPlayed(episode)
+            continue
+
+        if have < keep:
+            if get_episode(feed.name, episode, dl_dir):
+                have += 1
+        else:
+            delete_episode(feed.name, episode, dl_dir)
+
+    # Post-processing Put podcast in config file it was just a URL
+    if (not 'name' in feed):
+        feed['name'] = rss.feed['title']
+        logging.info("Updating name in config")
+
+    update_configfile(conf)
+
+def sync_external():
+
+    rsync_dir = directories['rsync']
+    dl_dir = directories['dl']
+
+    if os.path.exists(rsync_dir):
+        if ui.yesno(f'Sync to player {rsync_dir}?'):
+            run(['rsync', '-vaxE', '--size-only', '--exclude=".*"', '--delete', '--progress', '--ignore-errors' ,
+                expanduser(dl_dir), rsync_dir],
+                capture_output=False)
+            logging.info('Done.')
+    else: 
+        logging.info(f'Rsync destination {rsync_dir} not connected.')
+
+def parse_dirs(config):
+    s = config.default_section
+    data_dir = config.get(s, 'Data directory', fallback = '=~/.local/share/podpuller')
+    dl_dir = config.get(s, 'Download directory', fallback = '~/Downloads/podcasts/')
+    rsync_dir = config.get(s, 'MP3 Player directory', fallback = '/Volumes/M500/podcasts/')
+
+    global directories
+    directories['data'] = os.path.expanduser(data_dir)
+    directories['dl'] = os.path.expanduser(dl_dir)
+    directories['rsync'] = os.path.expanduser(rsync_dir)
+
+def main():
+    
+    # Read feeds from config file
+    conf = configparser.ConfigParser()
+    conf.read(expanduser(config_filename))
+
+    # Parse file location directories
+    parse_dirs(conf)
+
+    # Init DB
+    init_db(directories['data'])
+    logging.debug(list(Episode.select()))
+    
+    # Check for quick mode
+    be_quick = False
+    if conf.get(conf.default_section, 'Always Quick', fallback=False) or \
+        ui.yesno('Quick mode (without asking for deletion)'):
+        be_quick = True
+        
+    # Process Feeds (except DEFAULT)
+    for feed_name in conf:
+        if feed_name != conf.default_section:
+            process_feed(feed_name, conf, be_quick)
+
+    logging.info('Done updating feeds.')
+
+    # Try to sync to external drive (mp3 player)
+    sync_external()
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ ui.py
@@ -0,0 +1,28 @@
+import logging
+import sys
+import os
+
+from bullet import Check, colors
+
+# cli = Check(prompt = "Choose from the items below: ", 
+#     choices = ['yes', 'no'])  # Create a Bullet or Check object
+# result = cli.launch()  
+# print (result)
+
+def interrupt_dl():
+    logging.info("Download aborted by Ctrl+c")
+    try:
+        return yesno("Do you like to mark item as read? (y/n) or quit? (Ctrl+c): ")
+    except KeyboardInterrupt:
+        print("\nQuitting")
+        sys.exit()
+
+def yesno(prompt):
+    user_wish = input(prompt + ' [y/n]  ')
+    return user_wish and 'yes'.startswith(user_wish.lower()) 
+
+def mark_deletion(dir):
+    filenames = sorted(os.listdir(dir), reverse=True)
+    if filenames:
+        cli = Check(choices = filenames, check='X ', check_color=colors.foreground['red'])
+        return cli.launch()
